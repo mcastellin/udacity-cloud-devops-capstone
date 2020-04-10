4526042d487a36d7e790ad25f86eb09323efab5f
Manuel Castellin <manuel@castellinconsulting.com>
2020-04-10 16:28:31 +0100

    chore: finalize application documentation
    Describe how the pipeline work, plus including a failing smoke test to see the rollback phase in Jenkins

--- locustfile.py
+++ locustfile.py
@@ -1,12 +1,20 @@
 from locust import HttpLocust, TaskSet, between
 
+SAMPLE_REQUEST = {"text": "in three days"}
+
+
 def index(l):
     l.client.get("/")
 
-class PredictionLoadTesting(TaskSet):
-    tasks = {index: 1}
+
+def translation(l):
+    l.client.post("/translat", json=SAMPLE_REQUEST)
+
+
+class DateTranslationLoadTesting(TaskSet):
+    tasks = {index: 1, translation: 2}
 
 
 class WebsiteUser(HttpLocust):
-    task_set = PredictionLoadTesting
-    wait_time = between(0, 1)
+    task_set = DateTranslationLoadTesting
+    wait_time = between(0, 9)
